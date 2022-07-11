13c1f2a3de898b6033b45c76467eb078be6bebde
ripple <879258584@QQ.COM>
2022-07-11 10:46:30 +0800

    no message

--- SolutionDev/Objects/startup_stm32f429_439xx.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_stm32f429_439xx.o: Code\Driver\System\startup_stm32f429_439xx.s
--- arm1_freeRTOS/GD32F4xx_Firmware_Library/Template/Keil_project/output/startup_gd32f450_470.d
+++
@@ -1,1 +0,0 @@
-.\output\startup_gd32f450_470.o: ..\..\Firmware\CMSIS\GD\GD32F4xx\Source\ARM\startup_gd32f450_470.s
